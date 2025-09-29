4af0020d9a248aaf14b91b4e2ef805ce3669feca
rashed parvez <parvez08eee@gmail.com>
2025-09-29 13:33:31 +0600

    Db query works as expected

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,6 @@
 import {
-  FetchHttpClient,
   HttpApi,
   HttpApiBuilder,
-  HttpApiClient,
   HttpApiEndpoint,
   HttpApiGroup,
   HttpApiSwagger,
@@ -10,7 +8,6 @@ import {
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
 import { Effect, Layer, Schema, Console } from "effect";
 import { createServer } from "node:http";
-import { AppConfig, AppConfigLive } from "./config/Config";
 import { Db, DbLive } from "./config/Db";
 
 const UserScheme = Schema.Struct({
@@ -24,17 +21,35 @@ type User = Schema.Schema.Type<typeof UserScheme>;
 // create httpApi
 const MyApi = HttpApi.make("MyApi").add(
   HttpApiGroup.make("greet")
-    .add(HttpApiEndpoint.get("hello", "/hellow").addSuccess(UserScheme))
+    .add(HttpApiEndpoint.get("hello", "/hello").addSuccess(UserScheme))
     .add(HttpApiEndpoint.get("goodbye", "/goodbuye").addSuccess(Schema.String)),
 );
 
 const greetLive = HttpApiBuilder.group(MyApi, "greet", (handlers) =>
-  handlers.handle("hello", () =>
-    Effect.gen(function* () {
-      const sql = yield* Db;
-      const user = yield* sql`SELECT username, email, password FROM USER WHERE email = ${"admin@admin.com"} `;
-
-      return user[0] as User;
-    }),
-  ).handle("goodbye", () => Effect.succeed("Goodbye"),
+  handlers
+    .handle("hello", () =>
+      Effect.gen(function* () {
+        const sql = yield* Db;
+        const user =
+          yield* sql`SELECT username, email, password_hash as password FROM users WHERE email = ${"admin@admin.com"}`;
+        return user[0] as User;
+      }).pipe(
+        Effect.catchAll(() =>
+          Effect.succeed({ email: "", username: "", password: "" } as User),
+        ),
+      ),
+    )
+    .handle("goodbye", () => Effect.succeed("Goodbye")),
 );
+
+const MyApiLive = HttpApiBuilder.api(MyApi).pipe(Layer.provide(greetLive));
+
+const ServerLive = HttpApiBuilder.serve().pipe(
+  Layer.provide(HttpApiSwagger.layer()),
+  Layer.provide(MyApiLive),
+  Layer.provide(DbLive),
+  Layer.tap(() => Console.log("Server listenning at port 3002")),
+  Layer.provide(NodeHttpServer.layer(createServer, { port: 3002 })),
+);
+
+Layer.launch(ServerLive).pipe(NodeRuntime.runMain);
