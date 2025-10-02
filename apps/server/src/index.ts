4a8661c729e39b4b83e8dd990ef18b4bb6b3df9d
rashed parvez <parvez08eee@gmail.com>
2025-10-02 15:41:49 +0600

    demo server for now, will change after actual implmentation.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,8 @@
 import {
+  FetchHttpClient,
   HttpApi,
   HttpApiBuilder,
+  HttpApiClient,
   HttpApiEndpoint,
   HttpApiGroup,
   HttpApiSwagger,
@@ -53,3 +55,14 @@ const ServerLive = HttpApiBuilder.serve().pipe(
 );
 
 Layer.launch(ServerLive).pipe(NodeRuntime.runMain);
+
+const program = Effect.gen(function* () {
+  const client = yield* HttpApiClient.make(MyApi, {
+    baseUrl: "http://localhost:3002",
+  });
+
+  const hello = yield* client.greet["hello"]();
+  console.log(hello);
+});
+
+Effect.runFork(program.pipe(Effect.provide(FetchHttpClient.layer)));
