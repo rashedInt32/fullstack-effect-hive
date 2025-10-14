18a80da6d2d050327dae7a28fb61a89e105ca24e
rashed parvez <parvez08eee@gmail.com>
2025-10-14 12:50:11 +0600

    Add message schema and types

---
+++ packages/shared/src/schema/MessageSchema.ts
@@ -0,0 +1,50 @@
+import { Schema } from "effect";
+
+export const MessageServiceErrorSchema = Schema.Struct({
+  message: Schema.String,
+  code: Schema.Literal(
+    "MESSAGE_NOT_FOUND",
+    "MESSAGE_CREATION_FAILED",
+    "MESSAGE_VALIDATION_FAILED",
+    "MESSAGE_UPDATE_FAILED",
+    "MESSAGE_DELETE_FAILED",
+    "MESSAGE_ACCESS_DENIED",
+    "ROOM_NOT_FOUND",
+    "USER_NOT_IN_ROOM",
+    "INTERNAL_MESSAGE_ERROR",
+  ),
+});
+
+export const MessageRowSchema = Schema.Struct({
+  id: Schema.String,
+  room_id: Schema.String,
+  user_id: Schema.String,
+  content: Schema.String,
+  created_at: Schema.DateFromString,
+  updated_at: Schema.DateFromString,
+  deleted_at: Schema.Union(Schema.DateFromString, Schema.Null),
+});
+
+export const MessageCreateSchema = Schema.Struct({
+  room_id: Schema.String,
+  content: Schema.String.pipe(Schema.maxLength(4000), Schema.minLength(1)),
+});
+
+export const MessageUpdateSchema = Schema.Struct({
+  content: Schema.String.pipe(Schema.minLength(1), Schema.maxLength(4000)),
+});
+
+export const MessageSchema = MessageRowSchema.omit("deleted_at");
+
+export const MessageWithUserSchema = Schema.Struct({
+  ...MessageSchema.fields,
+  username: Schema.String,
+  user_email: Schema.Union(Schema.String, Schema.Null),
+  is_edited: Schema.Boolean,
+});
+
+export const MessageListQuerySchema = Schema.Struct({
+  room_id: Schema.String,
+  limit: Schema.optional(Schema.Number.pipe(Schema.int(), Schema.positive())),
+  before: Schema.optional(Schema.DateFromString),
+});
---
+++ packages/shared/src/types/MessageTypes.ts
@@ -0,0 +1,28 @@
+import { Schema } from "effect";
+import {
+  MessageCreateSchema,
+  MessageListQuerySchema,
+  MessageRowSchema,
+  MessageSchema,
+  MessageServiceErrorSchema,
+  MessageUpdateSchema,
+  MessageWithUserSchema,
+} from "../schema/MessageSchema";
+
+export type MessageServiceError = Schema.Schema.Type<
+  typeof MessageServiceErrorSchema
+>;
+
+export type MessageRow = Schema.Schema.Type<typeof MessageRowSchema>;
+
+export type Message = Schema.Schema.Type<typeof MessageSchema>;
+
+export type MessageCreate = Schema.Schema.Type<typeof MessageCreateSchema>;
+
+export type MessageUpdate = Schema.Schema.Type<typeof MessageUpdateSchema>;
+
+export type MessageListQuery = Schema.Schema.Type<
+  typeof MessageListQuerySchema
+>;
+
+export type MessageWithUser = Schema.Schema.Type<typeof MessageWithUserSchema>;
