59cb1bb8087b9e056dc4df4ab001e822db3e4ccc
rashed parvez <parvez08eee@gmail.com>
2025-11-18 12:02:53 +0600

    apiClient that holds all the api endpoints call

--- apps/web/lib/api/client.ts
+++ apps/web/lib/api/client.ts
@@ -2,7 +2,18 @@ import { tokenStorage } from "@/lib/api/storage";
 import { ApiError } from "@/lib/api/types";
 import { apiFetch } from "@/lib/apiFetch";
 import { Effect } from "effect";
-import { Room, RoomCreate, User, UserRow } from "@hive/shared";
+import {
+  Message,
+  MessageCreate,
+  MessageWithUser,
+  Room,
+  RoomCreate,
+  RoomMemberRow,
+  RoomUpdate,
+  RoomWithMembers,
+  User,
+  UserRow,
+} from "@hive/shared";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3002/api";
 
@@ -61,6 +72,65 @@ export const apiClient = {
       apiFetchWithAuth<Room>("/rooms/create", {
         method: "POST",
         body: JSON.stringify({ data }),
+      }),
+
+    getById: (id: string) =>
+      apiFetchWithAuth(`/rooms/${id}`, {
+        method: "GET",
+      }),
+
+    listByUser: (userId: string) =>
+      apiFetchWithAuth<RoomWithMembers[]>(`/rooms/user/${userId}`),
+
+    update: (roomId: string, data: Omit<RoomUpdate, "id">) =>
+      apiFetchWithAuth<Room>(`/rooms/update/${roomId}`, {
+        method: "PUT",
+        body: JSON.stringify({ data }),
+      }),
+
+    delete: (roomId: string) =>
+      apiFetchWithAuth<{ success: boolean; message: string }>(
+        `/rooms/${roomId}`,
+        {
+          method: "DELETE",
+        },
+      ),
+    listMembers: (roomId: string) =>
+      apiFetchWithAuth<RoomMemberRow[]>(`/rooms/${roomId}/members`, {
+        method: "GET",
+      }),
+    getMemberRole: (roomId: string, userId: string) =>
+      apiFetchWithAuth<"admin" | "owner" | "member">(
+        `/rooms/${roomId}/${userId}/role`,
+        {
+          method: "GET",
+        },
+      ),
+    isMember: (roomId: string, userId: string) =>
+      apiFetchWithAuth<boolean>(`/rooms/${roomId}/${userId}/isMember`, {
+        method: "GET",
+      }),
+  },
+
+  messages: {
+    create: (data: MessageCreate) =>
+      apiFetchWithAuth<Message>("/message/create", {
+        method: "POST",
+        body: JSON.stringify({ data }),
+      }),
+
+    listByRoom: (roomId: string) =>
+      apiFetchWithAuth<MessageWithUser[]>(`/message/room/${roomId}`),
+
+    update: (messageId: string, content: string) =>
+      apiFetchWithAuth<Message>(`/message/${messageId}`, {
+        method: "PUT",
+        body: JSON.stringify({ content }),
+      }),
+
+    delete: (messageId: string) =>
+      apiFetchWithAuth<void>(`/message/${messageId}`, {
+        method: "DELETE",
       }),
   },
 };
